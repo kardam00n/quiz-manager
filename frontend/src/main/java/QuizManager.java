57a130828c5877009a0820e57230b58406585aaa
Adam Mężydło <amezydlo@student.agh.edu.pl>
2023-12-08 11:16:59 +0100

    update frontend/build.gradle

---
+++ frontend/src/main/java/QuizManager.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class QuizManager extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("QuizManager");
+        primaryStage.setResizable(false);
+        primaryStage
+                .getIcons()
+                .add(new Image(Objects.requireNonNull(QuizManager.class.getResourceAsStream("logo.png"))));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
