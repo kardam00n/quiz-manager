9632bff9806ede6912e1b1c985ccd82caa2e031b
Adam Mężydło <amezydlo@student.agh.edu.pl>
2024-01-05 19:46:00 +0100

    finish strategy view (ważne że działa :))

--- frontend/src/main/java/quizmanager/model/StrategyDto.java
+++ frontend/src/main/java/quizmanager/model/StrategyDto.java
@@ -1,11 +1,25 @@
 package quizmanager.model;
 
 public class StrategyDto {
+    public StrategyDto() {
+
+    }
+
+    public StrategyDto(String n) {
+        algorithmName= n;
+    }
 
     String algorithmName;
 
     public void setAlgorithmName(String algorithmName) {
         this.algorithmName = algorithmName;
+    }
+
+    @Override
+    public String toString() {
+        return "StrategyDto{" +
+                "algorithmName='" + algorithmName + '\'' +
+                '}';
     }
 
     public String getAlgorithmName() {
--- frontend/src/main/java/quizmanager/presenter/StrategyConfigPresenter.java
+++ frontend/src/main/java/quizmanager/presenter/StrategyConfigPresenter.java
@@ -1,6 +1,5 @@
 package quizmanager.presenter;
 
-import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
@@ -8,6 +7,7 @@ import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import quizmanager.model.PrizeTypeDto;
 import quizmanager.model.StrategyDto;
 
 public class StrategyConfigPresenter {
@@ -37,12 +37,27 @@ public class StrategyConfigPresenter {
     private String quizTitle;
 
 
-
-
-
-
     @FXML
     private void initialize() {
+
+
+        chosenStrategy.getItems().addAll(new StrategyDto("one"), new StrategyDto("two"));
+        chosenStrategy.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> {
+                    StrategyDto selectedStrategy = chosenStrategy
+                            .getSelectionModel()
+                            .getSelectedItem();
+
+                    if (selectedStrategy.getAlgorithmName().equals("one")) {
+                        displayStrategyAControls();
+                    } else if (selectedStrategy.getAlgorithmName().equals("two")) {
+                        displayBStrategyControls();
+                    }
+
+                });
+
+        chosenStrategy.getSelectionModel().select(0);
+
 //        okButton.disableProperty().bind(
 //                Bindings.isEmpty(treshold.textProperty())
 //                        .or(Bindings.isEmpty(victoryPrizeType.textProperty()))
@@ -151,28 +166,105 @@ public class StrategyConfigPresenter {
 //
 //    }
 
-    public void displayBStrategyControls() {
 
+
+    private void displayStrategyAHeader() {
+        optionsPane.getChildren().remove(0, optionsPane.getChildren().size() - 1);
+
+
+        HBox title = new HBox();
+        title.setAlignment(Pos.CENTER);
+        title.setSpacing(20.0);
+
+        Label label = new Label("Procent najlepszych:");
+        label.setAlignment(Pos.CENTER);
+
+        Label label2 = new Label("Nagroda dla najlepszych:");
+        label2.setAlignment(Pos.CENTER);
+
+        Label label3 = new Label("Nagroda dla pozostałych:");
+        label3.setAlignment(Pos.CENTER);
+
+        title.getChildren().addAll(label, label2, label3);
+
+        optionsPane.getChildren().add(optionsPane.getChildren().size() - 1, title);
+
+    }
+
+    private void displayStrategyARow() {
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(20.0);
+
+        Spinner<Integer> spinner = new Spinner<>(0, Integer.MAX_VALUE, 0);
+        spinner.setMaxWidth(130.0);
+        spinner.setMinWidth(130.0);
+        ChoiceBox<PrizeTypeDto> victoryPrizeCategory = new ChoiceBox<>();
+        victoryPrizeCategory.setMinWidth(150.0);
+        victoryPrizeCategory.setMaxWidth(150.0);
+        ChoiceBox<PrizeTypeDto> restPrizeCategory = new ChoiceBox<>();
+        restPrizeCategory.setMinWidth(150.0);
+        restPrizeCategory.setMaxWidth(150.0);
+
+        hBox.getChildren().addAll(spinner, victoryPrizeCategory, restPrizeCategory);
+
+        optionsPane.getChildren().add(optionsPane.getChildren().size() - 1, hBox);
+    }
+
+    private void displayStrategyAControls(){
+        addRowButton.setVisible(false);
+        displayStrategyAHeader();
+        displayStrategyARow();
+    }
+
+
+    private void displayStrategyBHeader() {
+        optionsPane.getChildren().remove(0, optionsPane.getChildren().size() - 1);
+
+
+        HBox title = new HBox();
+        title.setAlignment(Pos.CENTER);
+        title.setSpacing(20.0);
+
+        Label label = new Label("Poprawne odpowiedzi: ");
+        label.setAlignment(Pos.CENTER);
+
+        Label label2 = new Label("Kategoria nagrody: ");
+        label2.setAlignment(Pos.CENTER);
+
+        title.getChildren().addAll(label, label2);
+        HBox.setHgrow(label, javafx.scene.layout.Priority.ALWAYS);
+
+        optionsPane.getChildren().add(optionsPane.getChildren().size() - 1, title);
+
+    }
+
+    private void displayStrategyBRow() {
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(20.0);
+
+        Spinner<Integer> spinner = new Spinner<>(0, Integer.MAX_VALUE, 0);
+        spinner.setMaxWidth(140.0);
+        spinner.setMinWidth(140.0);
+        ChoiceBox<PrizeTypeDto> prizeCategory = new ChoiceBox<>();
+        prizeCategory.setMinWidth(120.0);
+        prizeCategory.setMaxWidth(120.0);
+
+        hBox.getChildren().addAll(spinner, prizeCategory);
+
+        optionsPane.getChildren().add(optionsPane.getChildren().size() - 1, hBox);
+
+    }
+
+    private void displayBStrategyControls() {
+        addRowButton.setVisible(true);
+        displayStrategyBHeader();
+        displayStrategyBRow();
     }
 
     @FXML
     private void addRow(ActionEvent actionEvent) {
-        HBox hbox = new HBox();
-        hbox.setAlignment(javafx.geometry.Pos.CENTER);
-
-        hbox.setSpacing(20.0);
-
-
-        Label label = new Label("Próg: ");
-        label.setAlignment(Pos.CENTER);
-
-        TextField doubleTextField = new TextField();
-        doubleTextField.setAlignment(Pos.CENTER);
-
-
-        hbox.getChildren().addAll(label, doubleTextField);
-        HBox.setHgrow(label, javafx.scene.layout.Priority.ALWAYS);
-
-        optionsPane.getChildren().add(optionsPane.getChildren().size() - 1, hbox );
+        displayStrategyBRow();
     }
 }
