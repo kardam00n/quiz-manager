1d4b5d27364b38f2590f1bb2f3fc4e9a9484d3ac
Adam Mężydło <amezydlo@student.agh.edu.pl>
2024-01-19 00:24:25 +0100

    main view design complete

---
+++ frontend/src/main/java/quizmanager/presenter/MainPresenter.java
@@ -0,0 +1,11 @@
+package quizmanager.presenter;
+
+import de.jensd.fx.glyphs.GlyphsDude;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+
+public class MainPresenter {
+    public static void main(String[] args) {
+        FontAwesomeIcon.SUN
+    }
+}
